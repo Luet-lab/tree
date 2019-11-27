f54843bb2dc06137140302be347b7509587cdb53
Ettore Di Giacinto <mudler@gentoo.org>
2019-11-27 17:02:21 +0100

    Add base layers from gentoo stage3
    Add specfiles to create build layers for gentoo stage3 and portage tree pinning

---
+++ layers/base/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+apk --no-cache add ca-certificates tar wget xz rsync
+wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd64-systemd-20191123.tar.bz2 -O /rootfs.tar.bz2
+mkdir /gentoo && cd /gentoo && tar xpf /rootfs.tar.bz2 --xattrs --numeric-owner && rm /rootfs.tar.bz2
+rsync -A -a --delete --numeric-ids --recursive -d -H --one-file-system --xattrs --exclude '/gentoo/*'  --exclude '/etc/resolv.conf'  --exclude '/etc/hostname'  --exclude '/sys/' --exclude '/etc/hosts'  --exclude '/sys/*' --exclude '/proc/*' --exclude '/dev/pts/*' /gentoo/ /
+rm -rf /gentoo
