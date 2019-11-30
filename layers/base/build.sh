d0d3f750c7898f25f9e7ff0d7c4b708a3e7ce6fb
Daniele Rondina <geaaru@sabayonlinux.org>
2019-11-30 16:44:35 +0100

    base/build.sh: Retrieve stage3 version dynamically

--- layers/base/build.sh
+++ layers/base/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -ex
 apk --no-cache add ca-certificates tar wget xz rsync
-wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd64-systemd-20191123.tar.bz2 -O /rootfs.tar.bz2
+version=$(wget -O -  http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-systemd.txt --quiet | tail -n 1 | awk 'match($1, /^[0-9]+/)  { print substr($1, RSTART, RLENGTH) }')
+wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd64-systemd-${version}.tar.bz2 -O /rootfs.tar.bz2
 mkdir /gentoo && cd /gentoo && tar xpf /rootfs.tar.bz2 --xattrs --numeric-owner && rm /rootfs.tar.bz2
 rsync -A -a --delete --numeric-ids --recursive -d -H --one-file-system --xattrs --exclude '/gentoo/*'  --exclude '/etc/resolv.conf'  --exclude '/etc/hostname'  --exclude '/sys/' --exclude '/etc/hosts'  --exclude '/sys/*' --exclude '/proc/*' --exclude '/dev/pts/*' /gentoo/ /
 rm -rf /gentoo
