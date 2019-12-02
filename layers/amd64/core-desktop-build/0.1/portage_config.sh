0e73358e677a06de1be2829ebf0cabff44b1cd1e
Ettore Di Giacinto <mudler@gentoo.org>
2019-12-02 17:08:12 +0100

    Drop symlinks for img

--- layers/amd64/core-desktop-build/0.1/portage_config.sh
+++ layers/amd64/core-desktop-build/0.1/portage_config.sh
@@ -6,8 +6,8 @@ cd /opt/sabayon-build/conf/intel/portage
 # keep your specific stuff in "myconf" branch:
 git checkout -b myconf
 # symlink to your <arch>:
-ln -sf make.conf.amd64 make.conf
-ln -sf package.env.amd64 package.env
+cp -rfv make.conf.amd64 make.conf
+cp -rfv package.env.amd64 package.env
 # add & commit
 git add make.conf package.env
 git config --global user.name "root"
@@ -18,4 +18,4 @@ cd /etc/
 mv portage portage-gentoo
 #mv make.conf make.conf-gentoo
 # symlink to sabayon /etc/make.conf /etc/portage/:
-ln -sf /opt/sabayon-build/conf/intel/portage portage
+cp -rfv /opt/sabayon-build/conf/intel/portage portage
