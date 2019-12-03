7fbdc8dae8605f34a06bc1f15d95dc5a8a85e2f3
Daniele Rondina <geaaru@sabayonlinux.org>
2019-12-03 12:08:37 +0100

    base-funtoo/9999: Fix build.sh

--- layers/amd64/base-funtoo/9999/build.sh
+++ layers/amd64/base-funtoo/9999/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -ex
 apk --no-cache add ca-certificates tar wget xz rsync
-wget wget https://build.funtoo.org/funtoo-current/x86-64bit/generic_64/stage3-latest.tar.xz -O /rootfs.tar.bz2
-mkdir /gentoo && cd /gentoo && tar xpf /rootfs.tar.bz2 --xattrs --numeric-owner && rm /rootfs.tar.bz2
-rsync -A -a --delete --numeric-ids --recursive -d -H --one-file-system --xattrs --exclude '/gentoo/*'  --exclude '/etc/resolv.conf'  --exclude '/etc/hostname'  --exclude '/sys/' --exclude '/etc/hosts'  --exclude '/sys/*' --exclude '/proc/*' --exclude '/dev/pts/*' /gentoo/ /
-rm -rf /gentoo
+# NOTE: For now use zen variant.
+wget https://build.funtoo.org/1.4-release-std/x86-64bit/amd64-zen/stage3-latest.tar.xz -O /rootfs.tar.xz
+mkdir /funtoo && cd /funtoo && tar xJpf /rootfs.tar.xz --xattrs --numeric-owner && rm /rootfs.tar.xz
+ls -l /funtoo/
+rsync -A -a --delete --numeric-ids --recursive -d -H --one-file-system --xattrs --exclude '/funtoo/*'  --exclude '/etc/resolv.conf'  --exclude '/etc/hostname'  --exclude '/sys/' --exclude '/etc/hosts'  --exclude '/sys/*' --exclude '/proc/*' --exclude '/dev/pts/*' /funtoo/ /
+rm -rf /funtoo
