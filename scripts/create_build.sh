64d4e414d60fae0837300007dc5fdf6662b19124
Ettore Di Giacinto <mudler@gentoo.org>
2019-12-06 18:08:13 +0100

    Add maintenance scripts

---
+++ scripts/create_build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+LUET_REPO="${LUET_REPO:-/root/repo}"
+BUILD_LAYER_NAME="${BUILD_LAYER_NAME:-core-desktop-build}"
+BUILD_LAYER_CATEGORY="${BUILD_LAYER_CATEGORY:-layer}"
+BUILD_LAYER_VERSION="${BUILD_LAYER_VERSION:->=0.1}"
+
+gen_build() {
+local f=$1
+
+# use luet expansions
+PACKAGE_NAME="$(/tmp/yq r $f name)"
+PACKAGE_VERSION="$(/tmp/yq r $f version)"
+PACKAGE_CATEGORY="$(/tmp/yq r $f category)"
+
+basedir="$(dirname $f)"
+cat <<EOF > $basedir/build.yaml
+steps:
+- emerge =\${PACKAGE_CATEGORY}/\${PACKAGE_NAME}-\${PACKAGE_VERSION}
+requires:
+- category: "$BUILD_LAYER_CATEGORY"
+  version:  "$BUILD_LAYER_VERSION"
+  name:     "$BUILD_LAYER_NAME"
+- category: "layer"
+  version:  ">=0.1"
+  name:     "sabayon-overlay"
+EOF
+echo "Generated build definition for $PACKAGE_NAME-$PACKAGE_VERSION ($PACKAGE_CATEGORY)"
+}
+
+clean() {
+  rm -rf /tmp/yq
+}
+
+echo "Running builder creator script"
+
+if [ ! -e "/tmp/yq" ]; then
+wget 'https://github.com/mikefarah/yq/releases/download/2.4.1/yq_linux_amd64' -O /tmp/yq
+chmod +x /tmp/yq
+trap clean EXIT
+fi
+
+for f in $(find ${LUET_REPO} -name '*.yaml'); do gen_build $f; done
+
---
+++ scripts/sanitize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+LUET_REPO="${LUET_REPO:-/root/repo}"
+echo "Running sanitizer script"
+
+# Drop noisy output from yaml
+find "${LUET_REPO}" -type f -print0 | xargs -0 sed -i 's/State: .*//g'
+find "${LUET_REPO}" -type f -print0 | xargs -0 sed -i 's/set: false//g'
+find "${LUET_REPO}" -type f -print0 | xargs -0 sed -i 's/.*: null//g'
+find "${LUET_REPO}" -type f -print0 | xargs -0 sed -i '/^$/d'
+
+
+
