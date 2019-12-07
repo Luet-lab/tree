bc48260ca3b115abf47ccfc6eaacecf5e056951b
Ettore Di Giacinto <mudler@gentoo.org>
2019-12-07 17:15:41 +0100

    Use abs path

--- scripts/sanitize.sh
+++ scripts/sanitize.sh
@@ -5,10 +5,10 @@ LUET_REPO="${LUET_REPO:-/root/repo}"
 echo "Running sanitizer script"
 
 # Drop noisy output from yaml
-find "${LUET_REPO}" -path ./.git -prune -o -type f -print0 | xargs -0 sed -i 's/State: .*//g'
-find "${LUET_REPO}" -path ./.git -prune -o -type f -print0 | xargs -0 sed -i 's/set: false//g'
-find "${LUET_REPO}" -path ./.git -prune -o -type f -print0 | xargs -0 sed -i 's/.*: null//g'
-find "${LUET_REPO}" -path ./.git -prune -o -type f -print0 | xargs -0 sed -i '/^$/d'
+find "${LUET_REPO}" -path "$LUET_REPO"/.git -prune -o -type f -print0 | xargs -0 sed -i 's/State: .*//g'
+find "${LUET_REPO}" -path "$LUET_REPO"/.git -prune -o -type f -print0 | xargs -0 sed -i 's/set: false//g'
+find "${LUET_REPO}" -path "$LUET_REPO"/.git -prune -o -type f -print0 | xargs -0 sed -i 's/.*: null//g'
+find "${LUET_REPO}" -path "$LUET_REPO"/.git -prune -o -type f -print0 | xargs -0 sed -i '/^$/d'
 
 
 
