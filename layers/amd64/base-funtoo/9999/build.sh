6c0c405cbba0648815e6f985f82a4df91bf30201
Daniele Rondina <geaaru@sabayonlinux.org>
2019-12-07 14:38:47 +0100

    layers/amd64/base-funtoo/9999: Use generic_amd64 stage

--- layers/amd64/base-funtoo/9999/build.sh
+++ layers/amd64/base-funtoo/9999/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
 apk --no-cache add ca-certificates tar wget xz rsync
-# NOTE: For now use zen variant.
-wget https://build.funtoo.org/1.4-release-std/x86-64bit/amd64-zen/stage3-latest.tar.xz -O /rootfs.tar.xz
+# NOTE: For now use generic_amd64 variant.
+wget https://build.funtoo.org/1.4-release-std/x86-64bit/generic_64/stage3-latest.tar.xz -O /rootfs.tar.xz
 mkdir /funtoo && cd /funtoo && tar xJpf /rootfs.tar.xz --xattrs --numeric-owner && rm /rootfs.tar.xz
 ls -l /funtoo/
 rsync -A -a --delete --numeric-ids --recursive -d -H --one-file-system --xattrs --exclude '/funtoo/*'  --exclude '/etc/resolv.conf'  --exclude '/etc/hostname'  --exclude '/sys/' --exclude '/etc/hosts'  --exclude '/sys/*' --exclude '/proc/*' --exclude '/dev/pts/*' /funtoo/ /
