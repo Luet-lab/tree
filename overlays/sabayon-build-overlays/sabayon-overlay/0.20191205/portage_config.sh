331b8bba9be29cf09a9da4d4fd6f2b7ddcd6f0fa
Ettore Di Giacinto <mudler@gentoo.org>
2019-12-11 12:58:47 +0100

    Fixup setup of layer

--- overlays/sabayon-build-overlays/sabayon-overlay/0.20191205/portage_config.sh
+++ overlays/sabayon-build-overlays/sabayon-overlay/0.20191205/portage_config.sh
@@ -23,9 +23,13 @@ cp -rfv /opt/sabayon-build/conf/intel/portage portage
 
 # Drop portdir, we use default here
 sed -i 's/PORTDIR.*//g' /etc/portage/make.conf
-sed -i 's|/usr/portage.*|/var/db/repos/gentoo|g' /etc/portage/repos.conf/gentoo.conf || true 
+sed -i 's|/usr/portage.*|/var/db/repos/gentoo|g' /etc/portage/repos.conf/gentoo.conf || true
+echo 'PORTDIR="/var/db/repos/gentoo"' >> /etc/portage/make.conf
+sed -i 's/repos\.conf/make.conf/' /etc/layman/layman.cfg
+rm -rf /var/lib/layman/make.conf || true
+layman-updater -R
 eselect profile list
 # We need to select a profile here, because we just overwrote /etc/portage
-eselect profile set 16
+eselect profile set 5
 
 #emerge --update --newuse --deep --complete-graph @world
