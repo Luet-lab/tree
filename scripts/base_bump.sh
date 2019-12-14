319ca7b27dc00fd82614e5c8ceea2b5eea041acd
Ettore Di Giacinto <mudler@gentoo.org>
2019-12-14 23:41:27 +0100

    Also generate a pinned portage along with the base bump

--- scripts/base_bump.sh
+++ scripts/base_bump.sh
@@ -8,6 +8,7 @@ version=$(wget -4 -O -  http://distfiles.gentoo.org/releases/amd64/autobuilds/la
 echo "Generating base layer spec for $version"
 basedir=$ROOT_DIR/layers/amd64/base/0.$version
 
+if [ ! -d $basedir ]; then
 mkdir -p $basedir
 cp -rf $ROOT_DIR/layers/amd64/base/build.sh $basedir/
 mottainai-cli task compile "$ROOT_DIR"/templates/baselayer.tmpl \
@@ -19,3 +20,42 @@ mottainai-cli task compile "$ROOT_DIR"/templates/definition.yaml.tmpl \
                                 -s PackageVersion="0.$version" \
                                 -s PackageName="base" \
                                 -o $basedir/definition.yaml
+
+# Create a temporary directory and store its name in a variable ...
+TMPDIR=$(mktemp -d)
+
+# Bail out if the temp directory wasn't created successfully.
+if [ ! -e $TMPDIR ]; then
+    >&2 echo "Failed to create temp directory"
+    exit 1
+fi
+
+# Make sure it gets removed even if the script exits abnormally.
+trap "exit 1"           HUP INT PIPE QUIT TERM
+trap 'rm -rf "$TMPDIR"' EXIT
+
+git clone https://github.com/gentoo/gentoo $TMPDIR
+pushd $TMPDIR
+SHA=$(git rev-parse origin/master)
+popd
+
+d=`date +%Y%m%d`
+
+echo "Generating portage spec for $d - $SHA"
+basedir=$ROOT_DIR/overlays/portage/0.$d
+
+mkdir -p $basedir
+
+mottainai-cli task compile "$ROOT_DIR"/templates/portage.yaml.tmpl \
+                            -s LayerCategory="layer" \
+                            -s LayerVersion=0.$version \
+                            -s LayerName="base" \
+                            -s Commit="$SHA" \
+                            -o $basedir/build.yaml
+
+mottainai-cli task compile "$ROOT_DIR"/templates/definition.yaml.tmpl \
+                                -s PackageCategory="layer" \
+                                -s PackageVersion="0.$d" \
+                                -s PackageName="portage" \
+                                -o $basedir/definition.yaml
+fi
