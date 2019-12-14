acbea3d61c377513965bf2804a0feb05fa72da42
Ettore Di Giacinto <mudler@gentoo.org>
2019-12-14 20:21:56 +0100

    Track kernel modules currently available in entropy

--- scripts/gen_kernel_modules.sh
+++ scripts/gen_kernel_modules.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 # Kernel module bump
 
-MODULES=(net-vpn@wireguard@0.0.20190406-r1)
+MODULES=(
+"net-vpn@wireguard@0.0.20190406-r1"
+"app-emulation@virtualbox-guest-additions@6.0.14"
+"app-emulation@virtualbox-modules@6.0.14"
+"app-laptop@tp_smapi@0.43"
+"media-video@v4l2loopback@0.12.1"
+"net-firewall@rtsp-conntrack@4.18"
+"net-wireless@broadcom-sta@6.30.223.271-r6"
+"sys-fs@vhba@20190410"
+"sys-fs@zfs-kmod@0.8.2"
+"sys-power@acpi_call@3.17"
+"sys-power@bbswitch@0.8-r2"
+"x11-drivers@nvidia-drivers@430.26"
+"x11-drivers@nvidia-drivers@390.87-r2"
+"x11-drivers@nvidia-drivers@340.107-r1"
+)
+
 KERNELS=$(ls $ROOT_DIR/sys-kernel/linux-sabayon)
 
 for i in $KERNELS;
 do
-    for m in $MODULES;
+    for m in ${MODULES[@]};
     do
         parts=($(echo $m | tr "@" "\n"))
         cat=${parts[0]}
