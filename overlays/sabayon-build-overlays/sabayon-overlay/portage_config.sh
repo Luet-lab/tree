3c3f322c34d29314754ba8cd60b1d2c663c64c05
Daniele Rondina <geaaru@sabayonlinux.org>
2019-12-29 11:18:23 +0100

    layer/sabayon-build-overlays: Try to use sabayon profile

--- overlays/sabayon-build-overlays/sabayon-overlay/portage_config.sh
+++ overlays/sabayon-build-overlays/sabayon-overlay/portage_config.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 # fetch the bits!
-cd /opt
-git clone git://github.com/Sabayon/build.git sabayon-build
-cd /opt/sabayon-build/conf/intel/portage
-# keep your specific stuff in "myconf" branch:
-git checkout -b myconf
-# symlink to your <arch>:
-cp -rfv make.conf.amd64 make.conf
-cp -rfv package.env.amd64 package.env
-# add & commit
-git add make.conf package.env
-git config --global user.name "root"
-git config --global user.email "root@localhost"
-git commit -m "saving my configurations"
-# rename the gentoo /etc/make.conf and /etc/portage/:
-cd /etc/
-mv portage portage-gentoo
-#mv make.conf make.conf-gentoo
-# symlink to sabayon /etc/make.conf /etc/portage/:
-cp -rfv /opt/sabayon-build/conf/intel/portage portage
 
+set_build_profile () {
+  cd /opt
+  git clone git://github.com/Sabayon/build.git sabayon-build
+  cd /opt/sabayon-build/conf/intel/portage
+  # keep your specific stuff in "myconf" branch:
+  git checkout -b myconf
+  # symlink to your <arch>:
+  cp -rfv make.conf.amd64 make.conf
+  cp -rfv package.env.amd64 package.env
+  # add & commit
+  git add make.conf package.env
+  git config --global user.name "root"
+  git config --global user.email "root@localhost"
+  git commit -m "saving my configurations"
+  # rename the gentoo /etc/make.conf and /etc/portage/:
+  cd /etc/
+  mv portage portage-gentoo
+  #mv make.conf make.conf-gentoo
+  # symlink to sabayon /etc/make.conf /etc/portage/:
+  cp -rfv /opt/sabayon-build/conf/intel/portage portage
+}
+
+# Uncomment this to enable sabayon-build profile instead of sabayon profile
+# from sabayon-distro overlay
+# set_build_profile
 
 # Drop portdir, we use default here
 sed -i 's/PORTDIR.*//g' /etc/portage/make.conf
